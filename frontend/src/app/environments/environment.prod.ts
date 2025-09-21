7350ff929cc30a7d63a0bc6f1b68c1e071e2483b
Kamal El-Shazly <kamal.elshazly@izam.co>
2025-09-21 22:58:30 +0300

    use environments in the frontend

---
+++ frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://crop-caster-backend.vercel.app',
+};
--- frontend/src/app/services/crop-prices.service.ts
+++ frontend/src/app/services/crop-prices.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CropDetails } from '../models/crop-details';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CropPricesService {
-  private apiUrl = '/api/prices';
+  private apiUrl = `${environment.baseUrl}/api/prices`;
 
   constructor(private http: HttpClient) {}
 
