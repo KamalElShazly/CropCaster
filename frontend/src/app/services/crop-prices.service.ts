559297c52d01ebcca96c08102d2fd12be2f541d7
Kamal El-Shazly <kamal.elshazly@izam.co>
2025-09-23 03:29:31 +0300

    fix environment

--- frontend/src/app/services/crop-prices.service.ts
+++ frontend/src/app/services/crop-prices.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CropDetails } from '../models/crop-details';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
